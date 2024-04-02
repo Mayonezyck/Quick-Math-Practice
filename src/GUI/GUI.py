e68d5b1e6bcf62c849e9a897ef48925aa3947c6a
Mayonezyck <yicheng_zhu@outlook.com>
2024-04-02 13:04:28 -0400

    new GUI components

--- src/GUI/GUI.py
+++ src/GUI/GUI.py
@@ -1,5 +1,6 @@
 import sys
-from src.GUI.ButtonAction import test, newQuestions
+from src.GUI.ButtonAction import test
+from src.ProbGen import probGen1
 # 1. Import QApplication and all the required widgets
 from PyQt6.QtWidgets import (
     QApplication,
@@ -12,24 +13,36 @@ from PyQt6.QtWidgets import (
     QFrame,
     QLineEdit,
     QVBoxLayout,
+    QHBoxLayout,
     QMessageBox,
     QWidget,
 )
+from PyQt6 import QtCore
 
-
+#The first window that shows up
 class Window(QMainWindow):
     def __init__(self):
         self.w = 1280; self.h = 720
         super().__init__(parent=None)
         self.setWindowTitle("QMainWindow")
-        self.layout = QVBoxLayout()
+        self.layout = QVBoxLayout(self)
+        self.layout.setAlignment(QtCore.Qt.AlignmentFlag.AlignCenter) 
+        bigTitle = QLabel(text='Calculation Practice')
+        bigTitle.setAlignment(QtCore.Qt.AlignmentFlag.AlignCenter)
+        bigTitle.setStyleSheet("QLabel { font-size: 60pt; }") 
+        self.layout.addWidget(bigTitle)
+        input_row_layout = QHBoxLayout()
+        input_row_layout.addWidget(QLabel(text='Number of practices'))
         self.textbox = QLineEdit()
-        self.layout.addWidget(self.textbox)
-
+        self.textbox.setMaximumWidth(30)
+        input_row_layout.addWidget(self.textbox)
+        input_row_layout.setContentsMargins(600, 0, 600, 0)
+        self.layout.addLayout(input_row_layout)
         self.button = QPushButton("Submit")
         self.button.clicked.connect(self._open_new_window)
         self.layout.addWidget(self.button)
         centralwidget = QWidget()
+        #centralwidget.setStyleSheet("background-color: lightgray;")  # Optional styling
         centralwidget.setLayout(self.layout)
         self.setCentralWidget(centralwidget)
         self.resize(self.w,self.h)
@@ -43,16 +56,15 @@ class Window(QMainWindow):
         menu = self.menuBar().addMenu("&Menu")
         menu.addAction("&Exit", self.close)
         
-
     def _createToolBar(self):
         tools = QToolBar()
         tools.addAction("Exit", self.close)
         self.addToolBar(tools)
 
     def _createStatusBar(self):
-        status = QStatusBar()
-        status.showMessage("I'm the Status Bar")
-        self.setStatusBar(status)
+        self.status = QStatusBar()
+        self.status.showMessage("Ready")
+        self.setStatusBar(self.status)
     
     def _open_new_window(self):
         try:
@@ -61,26 +73,67 @@ class Window(QMainWindow):
             self.new_window.show()
         except ValueError:
             error_dialog = QMessageBox.warning(self, "Error", "Please enter a valid number.")
+            self.status.showMessage('Error: invalid number input')
+            self.setStatusBar(self.status)
     
     def _test(self):
         test.test()
     
-    def _oneRound(self):
-        pass
-        #Take the number of exercises first.
-        #Generate a random seed
-        #Generate random exercises based on the random seed.
-        #Generate 
+
     
 class SecondWindow(QWidget):
-    def __init__(self, number):
+    def __init__(self, number, mod = 'gen1'):
         super().__init__()
+        self.Correct = 0
+        self.InCorrect = 0
         self.setWindowTitle("Second Window")
         self.layout = QVBoxLayout(self)
-
+        self.firstGo = True
         self.label = QLabel(f"You entered the number: {number}")
+        match mod:
+            case 'gen1':
+                self.ProbGen = probGen1.probGen1(number)
+        QuestionDic = self.ProbGen._generateQuestions()
         self.layout.addWidget(self.label)
+        self.QuestionDicIt = iter(QuestionDic.items())
+        self.QuestionLabel = QLabel('----')
+        self.layout.addWidget(self.QuestionLabel)
+        self.AnswerBox = QLineEdit()
+        self.layout.addWidget(self.AnswerBox)
+        self.nextButton = QPushButton(text='Next')
+        self.nextButton.clicked.connect(self._show_next_item)
+        self.layout.addWidget(self.nextButton)
+        self._show_next_item()
+        
 
+            
+    def _show_next_item(self):
+        try:
+            ans = -1
+            if not self.firstGo:
+                ans = int(self.AnswerBox.text())
+            else:
+                self.firstGo = False
+            key, value = self.QuestionDicIt.getKey()
+            
+            self._checkTF(ans,value)
+            self.QuestionLabel.setText(str(key))
+        except StopIteration:
+            # Reached the end
+            print(int(self.AnswerBox.text()))
+            self.nextButton.setEnabled(False)
+            self.firstGo = True
+            self.QuestionLabel.setText("End of dictionary")
+            print(f'Correct{self.Correct}, Incorrect{self.InCorrect}')
+        except ValueError:
+            print('value error')
+            
+    def _checkTF(self, v1, v2):
+        print(v1, v2)
+        if v1 == v2:
+            self.Correct += 1
+        else:
+            self.InCorrect += 1
 def app():
     app = QApplication([])
     window = Window()
---
+++ src/ProbGen/probGen1.py
@@ -0,0 +1,36 @@
+import random
+class probGen1():
+    def __init__(self, n, difficulty=0):
+        self.n = n
+        self.difficulty = difficulty
+        operation = ['+', '-', '*'] #This difficulty doesn't have division
+        self.operation = operation[difficulty]
+    
+    def _generateQuestions(self):
+        Questions = {}
+        for _ in range(self.n):
+            num1 = random.randint(1,9)
+            num2 = random.randint(1,9)
+            op = random.randint(0,self.difficulty)
+            operation = self.operation[op]
+            match operation:
+                case '+':
+                    key = str(num1) + ' + ' + str(num2)
+                    val = num1 + num2
+                case '-':
+                    if not num1 > num2:
+                        temp = num2
+                        num2 = num1
+                        num1 = temp
+                    key = str(num1) + ' - ' + str(num2)
+                    val = num1 - num2
+                case '*':
+                    key = str(num1) + ' * ' + str(num2)
+                    val = num1 * num2
+            Questions[key] = val
+        return Questions
+
+                
+    def _getOperation(self):
+        return self.operation
+    
